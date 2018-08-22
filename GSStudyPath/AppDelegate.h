2cc2aa5d8e83af5db3dd8ceedb75179af457c0a8
gzp <zswangzp@163.com>
2018-08-22 18:07:48 +0800

    Initial Commit

---
+++ GSStudyPath/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GSStudyPath
+//
+//  Created by 管章鹏 on 2018/8/22.
+//  Copyright © 2018年 管章鹏. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GSStudyPath/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  GSStudyPath
+//
+//  Created by 管章鹏 on 2018/8/22.
+//  Copyright © 2018年 管章鹏. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
